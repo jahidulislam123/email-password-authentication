5a54da4f098d4530ce0b7950e84386d216896f3c
unknown <islam15-3879@diu.edu.bd>
2022-04-09 21:59:24 +0600

    form-added

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import app from './firebase.init';
+import { Button, Form } from 'react-bootstrap';
 
 
 
@@ -23,16 +24,28 @@ function App() {
       e.preventDefault();
     }
   return (
-    <div className="App">
-      <form onSubmit={handleFormSubmit} >
+    <div >
+      <div className='registration w-50 mx-auto mt-5' >
+        <h4  className='text-primary'> Please register</h4>
+      <Form onSubmit={handleFormSubmit} >
+  <Form.Group className="mb-3" controlId="formBasicEmail">
+    <Form.Label>Email address</Form.Label>
+    <Form.Control onBlur={handleEmailBlur} type="email" placeholder="Enter email" />
+    <Form.Text className="text-muted">
+      We'll never share your email with anyone else.
+    </Form.Text>
+  </Form.Group>
 
-      <input onBlur={handleEmailBlur} type="email" name="" id="" />
-      <br />
-        <input onBlur={handlePasswordBlur} type="password" name="" id="" />
-
-        <br />
-        <input type="submit" value="Login" />
-      </form>
+  <Form.Group className="mb-3" controlId="formBasicPassword">
+    <Form.Label>Password</Form.Label>
+    <Form.Control onBlur={handlePasswordBlur} type="password" placeholder="Password" />
+  </Form.Group>
+ 
+  <Button variant="primary" type="submit">
+    Submit
+  </Button>
+</Form>
+      </div>
 
     </div>
   );
