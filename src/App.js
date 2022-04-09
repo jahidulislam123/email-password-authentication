2d4ab2fb0bb69bf43a0cd9061c96bcaace0adecd
unknown <islam15-3879@diu.edu.bd>
2022-04-09 21:03:17 +0600

    added-bootstrap

--- src/App.js
+++ src/App.js
@@ -8,11 +8,30 @@ import app from './firebase.init';
 
 const auth=getAuth(app);
 function App() {
+
+
+  const handleEmailBlur=(e)=>{
+    console.log(e.target.value);
+  }
+
+  const handlePasswordBlur=(e)=>{
+    console.log(e.target.value);
+  }
+
+    const handleFormSubmit =(e)=>{
+      console.log('form submitted');
+      e.preventDefault();
+    }
   return (
     <div className="App">
-      <form>
-        <input type="text" />
-        <input type="password" name="" id="" />
+      <form onSubmit={handleFormSubmit} >
+
+      <input onBlur={handleEmailBlur} type="email" name="" id="" />
+      <br />
+        <input onBlur={handlePasswordBlur} type="password" name="" id="" />
+
+        <br />
+        <input type="submit" value="Login" />
       </form>
 
     </div>
